460721988fc1510f451aa26b98bfe86c0d8c3c93
khj68 <maga40@naver.com>
2020-08-05 17:15:30 +0900

    Read json file and return

--- back/ap/src/main/java/indoor/ap/web/WebRestController.java
+++ back/ap/src/main/java/indoor/ap/web/WebRestController.java
@@ -2,15 +2,34 @@ package indoor.ap.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileReader;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 @RestController
 public class WebRestController {
-    
+    JSONParser parser = new JSONParser();
+
     @GetMapping("/parse")
-    public String parse() {
-        return "parsed data";
+    public JSONArray parse() {
+        JSONArray j = new JSONArray();
+        try{
+            Object obj = parser.parse(new FileReader("C:\\Users\\SKTelecom\\Desktop\\jobs\\IN\\indoor-ap\\back\\ap\\src\\main\\java\\indoor\\ap\\web\\log\\200804_150811.json"));
+
+            JSONArray jsonArray = (JSONArray) obj;
+            return jsonArray;
+
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            return j;
+        }
+
+    
+        // return "parsed data";
     }
 
     @GetMapping("/spot")
