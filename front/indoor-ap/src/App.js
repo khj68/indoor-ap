1a5557f1a9184d817d3f533d7716118b8c44c95c
khj68 <maga40@naver.com>
2020-08-05 15:29:54 +0900

    Add circle api

--- front/indoor-ap/src/App.js
+++ front/indoor-ap/src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'
+import { RenderAfterNavermapsLoaded, NaverMap, Circle } from 'react-naver-maps'
+
+function CircleAPI(props) {
+  return (
+    <Circle
+      center={{ lat: props.lat, lng: props.lng }}
+      radius={1}
+      fillOpacity={0.5}
+      fillColor={'#FF0000'}
+      strokeColor={'red'}
+      clickable={true}
+      onClick={() => {
+        alert('clicked')
+      }}
+    />
+  )
+}
 
 function NaverMapAPI() {
   return (
@@ -11,21 +27,27 @@ function NaverMapAPI() {
         width: '100%',
         height: '85vh'
       }}
-      defaultCenter = {{ lat: 37.554722, lng: 126.970833 }}
-      defaultZoom={13}
-    />
+      defaultCenter = {{ lat: 37.511345, lng: 127.059788 }}
+      defaultZoom={19}
+    >
+    <CircleAPI lat='37.511345' lng='127.059788' />
+    </NaverMap>
   )
 }
 
 function App() {
   return (
+    <div>
     <RenderAfterNavermapsLoaded
       ncpClientId={'8nlf9bvios'}
       error={<p>Maps Load Error</p>}
       loading={<p>Maps Loading...</p>}
     >
       <NaverMapAPI />
+      <CircleAPI />
+
     </RenderAfterNavermapsLoaded>
+    </div>
   );
 }
 
